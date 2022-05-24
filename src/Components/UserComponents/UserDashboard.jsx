9a94316770dd58913cd84cc626c90c1a68fe6369
AnanyGupta-Deloitte <ananygupta8@deloitte.com>
2022-05-24 15:13:19 +0530

    Add files via upload

---
+++ src/Components/UserComponents/UserDashboard.jsx
@@ -0,0 +1,12 @@
+import Article from "./DashBoardComponents/Article";
+
+const UserDashboard = () => {
+    return ( 
+        <>
+        
+       <Article/> 
+        </>
+     );
+}
+ 
+export default UserDashboard;
